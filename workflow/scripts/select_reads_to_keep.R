717e03b9b33638c4dac004cbe7f41307ab9e9964
Jakob Peder Pettersen <jakobpeder.pettersen@gmail.com>
2025-10-20 13:13:51 +0200

    In the process of rewriting the selection script to use the R package chunked

--- workflow/scripts/select_reads_to_keep.R
+++ workflow/scripts/select_reads_to_keep.R
@@ -3,7 +3,8 @@ suppressMessages({
     library(purrr)
     library(glue)
     library(dplyr)
-    library(data.table)
+    library(chunked)
+    library(DBI)
 })
 
 log_progress <- function(msg) {
@@ -19,31 +20,73 @@ sample  <- args[[1L]]
 # bc_cutoff <- 7000L
 
 bc_cutoff  <- as.integer(args[[2L]])
-
+chunk_size  <- as.integer(args[[3L]])
+bc_names  <- glue("bc{1:3}")
 input_frequency_table  <- glue("results/{sample}/{sample}_frequency_table.txt")
-
 input_table_file <- glue("results/{sample}/{sample}_barcode_table.txt")
 
-bc_names  <- glue("bc{1:3}")
-
-log_progress("Reading barcode table")
-
-barcode_table  <- fread(file = input_table_file, header = TRUE, sep = "\t")
-
 log_progress("Reading frequency table")
-
 freq_table <- read.table(file = input_frequency_table, header = TRUE, row.names = NULL, sep = "\t")
+selected_freq_table <- freq_table[seq_len(min(bc_cutoff, nrow(freq_table))),]
+log_progress("Inititializing input barcode table stream")
+input_con <- file(input_table_file, open = "r")
+on.exit(close(input_con), add=TRUE)
 
-selected_freq_table <- freq_table[seq_len(bc_cutoff),]
+output_database_name <- glue("results/{sample}/{sample}_selected_barcode_table.sqlite")
+log_progress("Initializing output barcode database stream")
+output_db <- dbConnect(RSQLite::SQLite(), output_database_name)
+
+dbExecute(output_db, "DROP TABLE IF EXISTS selected_barcodes")
+dbExecute(
+    output_db,
+    "CREATE TABLE selected_barcodes (
+        read TEXT NOT NULL PRIMARY KEY,
+        UMI TEXT NOT NULL,
+        celltag TEXT NOT NULL
+    ) WITHOUT ROWID"
+)
+dbExecute(output_db, "CREATE INDEX idx ON selected_barcodes(read)")
+
+# dbWriteTable(output_db, "barcodes")
+on.exit(dbDisconnect(output_db), add = TRUE)
+
+
 
 log_progress("Selecting reads to keep")
-common_rows <- posDemux::row_match(barcode_table, selected_freq_table)
+read_table_chunkwise(file = input_table_file, header = TRUE, sep = "\t", chunk_size = chunk_size) |> 
+(\(table) filter(table, posDemux::row_match(table, selected_freq_table)))() |> 
+mutate(read, UMI, pasted_celltag = rlang::exec(!!! bc_names), .keep= "none") |> 
+write_chunkwise()
 
-pasted_celltag <- barcode_table[common_rows, do.call(paste, c(.SD, sep = "_")), .SDcols = bc_names]
-selected_barcode_table  <- data.table(read=barcode_table$read[common_rows], UMI=barcode_table$UMI[common_rows],
- celltag=pasted_celltag)
 
-kept_reads <- barcode_table$read[common_rows]
+header <- read.table(input_con, nrows = 0, header=TRUE,
+ colClasses = c("character","character","character"))
+repeat {
+    chunk <- read.table(
+        input_con,
+        nrows = chunk_size,
+        header = FALSE,
+        col.names = names(header),
+        colClasses = c("character","character","character")
+    )
+    if (!nrow(chunk)) break
+
+    common_rows <- posDemux::row_match(chunk, selected_freq_table)
+
+    pasted_celltag <- chunk  |>
+     filter(common_rows)  |>
+     select(!!! bc_names) |>
+      (\(x) do.call(paste, c(x, sep = "_")))()
+
+    
+    selected_barcode_table  <- data.frame(read=chunk$read[common_rows], UMI=chunk$UMI[common_rows],
+    celltag=pasted_celltag)
+    dbAppendTable(output_db, "selected_barcodes", selected_barcode_table)
+}
+
+
+
+
 
 log_progress("Writing processed frequency table")
 write.table(
@@ -52,12 +95,9 @@ write.table(
     col.names = TRUE
 )
 
-log_progress("Writing processed barcode table")
-data.table::fwrite(x = selected_barcode_table, file = "results/{sample}/{sample}_selected_barcode_table.txt" |> glue(), sep="\t", nThread = 1L)
-
 log_progress("DONE")
 
-n_selected <- sum(common_rows)
-n_read  <- nrow(barcode_table)
-selection_percentage <- n_selected/n_read*100
+n_read  <- freq_table$frequency |> sum()
+n_selected <- selected_freq_table$frequency |> sum()
+selection_percentage <- n_selected / n_read*100
 glue("Selected {n_selected} of {n_read} reads ({round(selection_percentage, 2L)}%) based on barcode frequency")
